c14aed0c35b28ea283f8199228b89fc8d2624bed
Bee <bee128029@protonmail.com>
2023-02-27 18:16:25 -0800

    Make output nicer and add cli flags
    These changes ended up happening together. Added cli flags for the
    total of all rolls (-t|--total) and for subtotals of dice groups
    (-s|--subtotals). Compound options are not yet working. The code
    also sucks now.

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,162 @@
+use std::collections::HashSet;
 use std::io::prelude::*;
 use std::io::stdout;
 use std::io::BufWriter;
+// use std::str::FromStr;
 
 use rand::prelude::*;
 
-fn main() {
+fn main() -> Result<(), ParseError> {
     let raw_args: Vec<String> = std::env::args().collect();
-    let args = raw_args.get(1..).expect("1.. is never out of bounds");
+    let args = raw_args
+        .get(1..)
+        .expect("1.. is never out of bounds")
+        .to_vec();
 
-    let dice = match input_to_dice(args) {
-        Some(d) => d,
-        None => {
-            eprintln!("Failed to parse dice");
-            return;
-        }
-    };
+    let (flags, dice) = parsed_args(args)?;
 
     let mut rng = thread_rng();
     let stdout = stdout();
     let mut buffer = BufWriter::new(stdout);
 
-    for (times, die) in dice {
-        assert!(die != 0, "Parser did not prevent zero-sided die");
+    match dice.len() {
+        // No args -> roll d20
+        0 => {
+            let roll = rng.gen_range(1..=20);
 
-        for _ in 0..times {
-            let roll = rng.gen_range(1..=die);
             buffer
-                .write_fmt(format_args!("d{die}: {roll}\n"))
+                .write_fmt(format_args!("{roll}\n"))
                 .expect("Write failed");
         }
+        1 => {
+            let mut total: u32 = 0;
+
+            for _ in 0..dice[0].amount {
+                let roll = rng.gen_range(1..=dice[0].sides);
+
+                buffer
+                    .write_fmt(format_args!("{roll}\n"))
+                    .expect("Write failed");
+                total += roll;
+            }
+
+            if flags.contains(&Flag::Total) {
+                buffer
+                    .write_fmt(format_args!("total: {total}\n"))
+                    .expect("Write failed");
+            }
+        }
+        _ => {
+            let mut total: u32 = 0;
+
+            for die in dice {
+                assert!(die.sides != 0, "Parser did not prevent zero-sided die");
+
+                let mut subtotal: u32 = 0;
+
+                buffer
+                    .write_fmt(format_args!("d{}:\n", die.sides))
+                    .expect("Write failed");
+
+                for _ in 0..die.amount {
+                    let roll = rng.gen_range(1..=die.sides);
+
+                    buffer
+                        .write_fmt(format_args!(" {roll}\n"))
+                        .expect("Write failed");
+                    subtotal += roll
+                }
+
+                if flags.contains(&Flag::Subtotals) {
+                    buffer
+                        .write_fmt(format_args!("d{} subtotal: {subtotal}\n", die.sides))
+                        .expect("Write failed");
+                }
+                total += subtotal;
+            }
+
+            if flags.contains(&Flag::Total) {
+                buffer
+                    .write_fmt(format_args!("total: {total}\n"))
+                    .expect("Write failed");
+            }
+        }
     }
 
-    buffer.flush().expect("Flush failed")
+    buffer.flush().expect("Flush failed");
+
+    Ok(())
 }
 
-// Parses command line args into groups of dice in form (ammount of dice, number of sides)
-// Matches empty input to 1 d20
-fn input_to_dice(args: &[String]) -> Option<Vec<(u32, u32)>> {
-    let mut dice: Vec<(u32, u32)> = vec![];
+struct DiceGroup {
+    amount: u32,
+    sides: u32,
+}
 
-    if args.is_empty() {
-        return Some(Vec::from([(1, 20)]));
-    }
+#[derive(PartialEq, Eq, std::hash::Hash)]
+enum Flag {
+    Total,
+    Subtotals,
+}
+
+#[derive(Debug)]
+enum ParseError {
+    Flag,
+    Dice,
+}
+
+// Parse cli args into the enabled flags and dice groups (# dice, # sides)
+fn parsed_args(args: Vec<String>) -> Result<(HashSet<Flag>, Vec<DiceGroup>), ParseError> {
+    let (str_flags, str_dice): (Vec<String>, Vec<String>) =
+        args.into_iter().partition(|a| (*a).starts_with('-'));
+
+    let flags = parsed_flags(str_flags)?;
+    let dice = parsed_dice(str_dice)?;
+
+    Ok((flags, dice))
+}
+
+fn parsed_flags(args: Vec<String>) -> Result<HashSet<Flag>, ParseError> {
+    let mut flags = HashSet::new();
 
     for arg in args {
-        let (times, die) = arg.split_once('d')?;
-
-        let times = if times.is_empty() {
-            1u32
-        } else {
-            times.parse::<u32>().ok()?
+        let flag = match arg.as_str() {
+            "--total" | "-t" => Flag::Total,
+            "--subtotals" | "-s" => Flag::Subtotals,
+            _ => return Err(ParseError::Flag),
         };
 
-        let die = die.parse::<u32>().ok()?;
-
-        if die == 0 {
-            return None;
-        }
-
-        dice.push((times, die))
+        flags.insert(flag);
     }
 
-    Some(dice)
+    Ok(flags)
+}
+
+// Parses command line args into groups of dice in form (amount of dice, number of sides)
+fn parsed_dice(args: Vec<String>) -> Result<Vec<DiceGroup>, ParseError> {
+    let mut dice = vec![];
+
+    for arg in args {
+        let (amount, sides) = arg.split_once('d').ok_or(ParseError::Dice)?;
+
+        let amount = amount.parse::<u32>().or_else(|error| match error.kind() {
+            std::num::IntErrorKind::Empty => Ok(1),
+            _ => Err(ParseError::Dice),
+        })?;
+
+        let sides = match sides.parse::<u32>() {
+            Ok(n) => n,
+            Err(_) => {
+                return Err(ParseError::Dice);
+            }
+        };
+
+        if sides == 0 {
+            return Err(ParseError::Dice);
+        }
+
+        dice.push(DiceGroup { amount, sides })
+    }
+
+    Ok(dice)
 }
